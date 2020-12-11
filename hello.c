713b69178be48fd561c6824654ebd543e0c28097
mssha94 <you@example.com>
2020-12-11 17:55:48 +0200

    Initial commit

---
+++ hello.c
@@ -0,0 +1,114 @@
+#include <stdio.h> // 
+ 
+int main () {
+
+              printf ("%10s.\n", "Work 1");printf ("\n");
+
+    printf ("cli: Hello World\n");
+
+       printf ("ser: %s\n", "Hello Misha");
+
+    printf ("cli: How old are you?\n");
+
+    	printf ("ser: %s\n","Me 2020");
+
+    	printf ("\n");
+
+                      printf ("%10s.\n", "Work 2");printf ("\n");
+
+//----------------------------------------------------------
+
+        printf("%d\n", 555);
+              printf("%10d\n", 555);
+        printf("%d\n", 555); // cheslo cile
+           printf ("\n");
+           printf ("\n");
+	
+//----------------------------------------------------------
+                    printf ("%10s.\n", "Work 2");printf ("\n");
+	   
+      printf("%s %10d.\n","Words:", 59);// ryadok
+
+	    printf("%s %8d.\n","Letters:", 1004);
+
+      printf("%s %9d.\n","Didits:", 8);
+
+      printf ("\n");
+
+                    printf ("%10s.\n", "Work 3");printf ("\n");
+//-------------------------------------------------------------------------------
+    int lines, i;
+    int count = 6;
+
+    lines = 101;
+    i = -5;
+    printf("%5d %5d %5d\n", i, count+3, lines);
+    printf ("\n");
+
+
+                    printf ("%10s.\n", "Work 4");printf ("\n");
+//----------------------------------------------------------------------------------
+    int max = 2147483647;
+
+        printf("%d\n", max+1);
+        printf("%d\n", max+3);
+        printf("%d\n", max+55);
+        printf ("\n");
+
+    printf ("%10s.\n", "Work 5");printf ("\n");
+//--------------------------------------------------------------------------
+    int min = -2147483648;
+
+    printf("%d\n", min-1);
+    printf("%d\n", min-2);
+    printf("%d\n", min-10);
+
+      printf ("\n"); printf ("\n"); printf ("\n");
+
+
+   /// #include <stdio.h>
+///main() {
+      printf ("%10s.\n", "Work 6");printf ("\n");
+    char ch = 63;
+    unsigned char uch = 'r';
+    short j = 'b', k = 99;
+
+        printf("%c == %d\n", ch, ch);
+        printf("%c == %d\n", uch, uch);
+        printf("%c, %c\n", j, k);
+
+//----------------------------------
+printf ("\n"); printf ("\n"); printf ("\n");
+ //---------------------------------------
+
+
+
+printf ("%10s.\n", "Work 7");printf ("\n");
+    
+    int a = 10;
+
+int b[10];
+int c[2];
+int d[1];
+
+
+
+        printf("Integer: %lu \n", sizeof(a));
+        printf("Unsigned: %lu \n", sizeof(a));
+        printf("Short: %lu \n", sizeof(a));
+        printf("Unsigned short: %lu \n", sizeof(a));
+        printf("Long: %lu \n", sizeof(a));
+        printf("Unsigned long: %lu \n", sizeof(float));
+        printf("Char: %lu \n", sizeof(float));
+        printf("Float: %lu \n", sizeof(float));
+        printf("Double: %lu \n", sizeof(float));
+        printf("Long double: %lu \n", sizeof(float));
+        printf("String <Hello>: %lu \n", sizeof(float));
+        printf("Array of 10 integers: %lu \n", sizeof(b));
+
+printf ("\n"); printf ("\n"); printf ("\n");
+}
+
+
+
+
